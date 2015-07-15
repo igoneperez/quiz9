b922dbd726307d38e51890c238cc100cee891b77
Igone Perez <aphernandez@APHERNANDEZPW7.indra.es>
2015-07-15 17:00:49 +0200

    Lista de Preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
-router.get('/author',   	   quizController.author);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
+// Definición de rutas de /author
+router.get('/author',   	   quizController.author);
 
 module.exports = router;
