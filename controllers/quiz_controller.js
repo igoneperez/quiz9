0044e5d8665d80a32b441af414a9841bcc30cbeb
Igone Perez <aphernandez@APHERNANDEZPW7.indra.es>
2015-08-02 22:37:17 +0200

    Crear Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -45,6 +45,25 @@ exports.answer = function(req, res) {
   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
 
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');  
+  })   // res.redirect: Redirección HTTP a lista de preguntas
+};
+
 // GET /author
 exports.author = function(req, res) {
    res.render('author', {name: 'Igone Pérez Hernández'});
