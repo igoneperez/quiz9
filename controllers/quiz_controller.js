685db2f650a41c51ea936e49cd96e3cffe947491
Igone Perez <aphernandez@APHERNANDEZPW7.indra.es>
2015-08-02 16:26:22 +0200

    Ejercicio Obligatorio Modulo7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,21 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
-    }
-  ).catch(function(error) { next(error);})
+	if (!req.query.search) {
+		//Si no se busca nada, se muestran todas las preguntas
+		models.Quiz.findAll().then( function(quizes) {
+			res.render('quizes/index', 
+				{	quizes: quizes }
+			);
+		}).catch( function(error) { next(error); });
+	} else {
+		var search = '%'+req.query.search.replace(' ', '%')+'%';
+		models.Quiz.findAll({where: ["pregunta like ?", search]}).then( function(quizes) {
+			res.render('quizes/index', 
+				{	quizes: quizes }
+			);
+		}).catch( function(error) { next(error); });
+	}
 };
 
 // GET /quizes/:id
