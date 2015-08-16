1ad0caef95e7d917fad8c4739ed0a9a643511314
Igone Perez <aphernandez@APHERNANDEZPW7.indra.es>
2015-08-16 22:50:11 +0200

    Moderacion de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,19 @@ var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
+
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
@@ -24,13 +30,10 @@ router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quiz
 router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
-router.get('/quizes/:quizId(\\d+)/comments/new',   commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',      commentController.create);
-
-// Definición de rutas de sesion
-router.get('/login',  sessionController.new);     // formulario login
-router.post('/login', sessionController.create);  // crear sesión
-router.get('/logout', sessionController.destroy); // destruir sesión
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+	                                    sessionController.loginRequired, commentController.publish);
 
 // Definición de rutas de /author
 
